f9403c2b482487568f30ce6a66f5fd7ed995614a
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-20 19:53:39 +0200

    Added memory

--- simple-gh-agent/src/mastra/agents/github-agent.ts
+++ simple-gh-agent/src/mastra/agents/github-agent.ts
@@ -1,10 +1,18 @@
 import { Agent } from "@mastra/core/agent";
 import { githubClient } from "../mcp/github-client";
+import { Memory } from "@mastra/memory";
+import { LibSQLStore } from "@mastra/libsql";
 
 export const githubAgent = new Agent({
     id: 'github-agent',
     name: 'agent',
     instructions: "You should perforn GitHub operations and answer GitHub-related questions",
     model: 'huggingface/MiniMaxAI/MiniMax-M2',
-    tools: await githubClient.listTools()
+    tools: await githubClient.listTools(),
+    memory: new Memory({
+        storage: new LibSQLStore({
+            id: 'storage-01',
+            url: 'file:./mastra.db'
+        })
+    })
 })
