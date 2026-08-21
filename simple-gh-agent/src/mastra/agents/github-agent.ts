7f79d98991477643247b52ed8e1391ed40f52ed4
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-08-21 11:04:18 +0200

    feat: update GitHub agent to use new OpenAI SDK and add dependencies

--- simple-gh-agent/src/mastra/agents/github-agent.ts
+++ simple-gh-agent/src/mastra/agents/github-agent.ts
@@ -2,12 +2,18 @@ import { Agent } from "@mastra/core/agent";
 import { githubClient } from "../mcp/github-client";
 import { Memory } from "@mastra/memory";
 import { LibSQLStore } from "@mastra/libsql";
+import { createOpenAI } from '@ai-sdk/openai'
+
+const featherless = createOpenAI({
+    baseURL: "https://api.featherless.ai/v1",
+    apiKey: process.env.FEATHERLESS_API_KEY!
+})
 
 export const githubAgent = new Agent({
     id: 'github-agent',
     name: 'agent',
     instructions: "You should perforn GitHub operations and answer GitHub-related questions",
-    model: 'huggingface/MiniMaxAI/MiniMax-M2',
+    model: featherless.chat('zai-org/GLM-5.2'),
     tools: await githubClient.listTools(),
     memory: new Memory({
         storage: new LibSQLStore({
